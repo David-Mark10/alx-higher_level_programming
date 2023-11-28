6669ab00f1664876bf465970a71bd81949a8b729
David-Mark10 <dm_20167@yahoo.com>
2023-11-28 04:53:58 -0800

    My first python project

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,24 @@
-#!/usr/bin/python3
-import sys
-sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
-sys.exit(1)
+#include "lists.h"
+
+/**
+ * check_cycle - This function checks if
+ * a linked list contains a cycle
+ * @list: It is argument that linked list to check
+ *
+ * Return: return 1 if the list contain a cycle, 0 if it doesn't
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *ptr = list;
+	listint_t *pt = list;
+
+	while (ptr && pt && pt->next)
+	{
+		ptr = ptr->next;
+		pt = pt->next->next;
+		if (ptr == pt)
+			return (1);
+	}
+
+	return (0);
+}
